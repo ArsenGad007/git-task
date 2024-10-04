50e0a0d966fb43da3db0a68481cfe745e25ab561
Arsen Gadzhiyev <arsen.gadzhiyev.00@mail.ru>
2024-10-04 21:48:38 +0300

    Add is leap year program

---
+++ DateTime.pas
@@ -0,0 +1,14 @@
+﻿begin
+  var year := ReadInteger('Введите год:');
+  Assert(year > 0);
+  
+  var res := False;
+  
+  if year mod 4 = 0 then
+    if year mod 100 <> 0 then
+      res := True
+    else if year mod 400 = 0 then
+      res := True;
+  
+  Print($'Год является високосным - {res}');
+end.
